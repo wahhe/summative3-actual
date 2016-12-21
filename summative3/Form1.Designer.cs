13da2adcca7faaa52c366f92737d423c7169db91
wahhe <yuanhe326@ed.amdsb.ca>
2016-12-21 13:26:24 -0500

    Add project files.

---
+++ summative3/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace summative3
+{
+    partial class summative
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.exploreButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.loadLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Orator Std", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(81, 72);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(253, 45);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "XXX MISSION";
+            // 
+            // exploreButton
+            // 
+            this.exploreButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.exploreButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.WhiteSmoke;
+            this.exploreButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.exploreButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exploreButton.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.exploreButton.Location = new System.Drawing.Point(112, 252);
+            this.exploreButton.Name = "exploreButton";
+            this.exploreButton.Size = new System.Drawing.Size(188, 65);
+            this.exploreButton.TabIndex = 1;
+            this.exploreButton.Text = "EXPLORE";
+            this.exploreButton.UseVisualStyleBackColor = false;
+            this.exploreButton.Click += new System.EventHandler(this.exploreButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.Black;
+            this.outputLabel.Location = new System.Drawing.Point(60, 44);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 23);
+            this.outputLabel.TabIndex = 2;
+            // 
+            // loadLabel
+            // 
+            this.loadLabel.AutoSize = true;
+            this.loadLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadLabel.Location = new System.Drawing.Point(115, 194);
+            this.loadLabel.Name = "loadLabel";
+            this.loadLabel.Size = new System.Drawing.Size(0, 25);
+            this.loadLabel.TabIndex = 3;
+            // 
+            // summative
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(420, 373);
+            this.Controls.Add(this.loadLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.exploreButton);
+            this.Controls.Add(this.titleLabel);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Name = "summative";
+            this.Text = "Summative3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button exploreButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label loadLabel;
+    }
+}
+
---
+++ summative3/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace summative3
+{
+    public partial class summative : Form
+    {
+        public summative()
+        {
+            InitializeComponent();
+        }
+
+        private void exploreButton_Click(object sender, EventArgs e)
+        {
+            titleLabel.Hide();
+            exploreButton.Hide();
+
+            Thread.Sleep(10);
+
+            Graphics XXX = CreateGraphics();
+            Graphics g = this.CreateGraphics();
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush purpleBrush = new SolidBrush(Color.Purple);
+            Pen whitePen = new Pen(Color.White);
+            //initialize
+            int x = 10;
+            int y = 10;
+
+            //test
+
+
+            while (y <= this.Width)
+            {
+
+                //act
+                //g.Clear(Color.White);
+                g.FillRectangle(whiteBrush, 0, 0, x, y);
+
+                Thread.Sleep(5);
+
+                //change
+                x++;
+                y = x + 2;
+            }
+
+            outputLabel.Text = "Welcome to the XXX world. ";
+            outputLabel.Refresh();
+            Thread.Sleep(2000);
+            outputLabel.Hide();
+            g.Clear(Color.Black);
+            g.DrawLine(whitePen, 0, 300, x, 300);
+            //this.width start from the right, x=0 start from the left
+            //when x < 0, X++ the line grow from left to right
+            for (x = 0; x < Width ; x++)
+            {
+                //act
+                g.Clear(Color.Black);
+                g.FillRectangle(blackBrush, x, 300, 10, 10);
+                g.DrawLine(whitePen, x, 300, 0, 300);
+
+                Thread.Sleep(5);
+            }
+
+
+            for (y = 290; y > 75; y--)
+            {
+                //act
+                g.Clear(Color.Black);
+                g.FillRectangle(whiteBrush, 200, y, 10, 10);                            
+                g.DrawLine(whitePen, 0, 300, x, 300);
+
+                Thread.Sleep(10);
+            }
+            
+            for (int pixels = 1; pixels < 256; pixels++)
+            {
+                //act
+                g.Clear(Color.Black);
+
+                whiteBrush.Color = Color.FromArgb(255 - pixels, 0 , 255 - pixels);
+
+                g.FillRectangle(whiteBrush, 200 - pixels / 2, 75 - pixels / 2, 10 + pixels, 10 + pixels);
+                g.DrawLine(whitePen, 0, 300, x, 300);
+
+                Thread.Sleep(5);
+            }
+
+            g.Clear(Color.Black );
+
+            for (x = 0; x < Width ; x++)
+            {
+                //act
+                g.Clear(Color.Black);
+                whiteBrush.Color = Color.White;
+                //g.FillRectangle(whiteBrush, x, 200, 10, 10);
+                g.DrawLine(whitePen, 0, 320, x, y);
+                g.FillRectangle(blackBrush, 220, 180, 30, 30);
+                g.DrawLine(whitePen, 220, 198, 220, 300);
+                g.DrawLine(whitePen, 250, 178, 250, 300);
+                g.DrawLine(whitePen, 220, 300, 250, 300);
+                g.FillRectangle(purpleBrush, 225, 275, 20, 20);
+                Thread.Sleep(10);
+            }
+
+
+            //for (x = 10; x < Width - 20; x = x+1)
+            //{
+                
+            //    g.FillRectangle(redBrush, x, y, 10, 10);
+              
+            //    Thread.Sleep(5);
+            //}
+
+            for (y = 0; y < 275; y++)
+            {
+                //act
+                
+                g.FillRectangle(redBrush, 225, y, 10, 10);
+              
+
+                Thread.Sleep(10);
+            }
+
+            for (int pixels = 0; pixels < 256; pixels++)
+            {
+                //act
+       
+
+                whiteBrush.Color = Color.FromArgb(255 - pixels, 0, 255 - pixels);
+
+                g.FillRectangle(whiteBrush, 225 - pixels / 2, 300 - pixels / 2, 30 + pixels, 30 + pixels);
+
+               
+                Thread.Sleep(5);
+            }
+
+            outputLabel.Text = "Mission explored";
+
+
+        }
+    }
+}
+
+            
+            
+        
+
+      
+
+    
+
